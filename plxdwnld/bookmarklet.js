b241b222ebafcbec036e0b8623e7afa940a56249
piplongrun <piplongrun@users.noreply.github.com>
2020-01-28 23:55:48 +0100

    Update bookmarklet.js

--- plxdwnld/bookmarklet.js
+++ plxdwnld/bookmarklet.js
@@ -4,25 +4,27 @@
  * This project is licensed under the terms of the MIT license, see https://piplongrun.github.io/plxdwnld/LICENSE.txt
  *
  * @author      Pip Longrun <pip.longrun@protonmail.com>
- * @version     0.1
+ * @version     0.2
  * @see         https://piplongrun.github.io/plxdwnld/
  *
  */
- var plxDwnld = (function() {
-    var self = {};
-    var clientIdRegex = new RegExp("server\/([a-f0-9]{40})\/");
-    var metadataIdRegex = new RegExp("key=%2Flibrary%2Fmetadata%2F(\\d+)");
-    var apiResourceUrl = "https://plex.tv/api/resources?includeHttps=1&X-Plex-Token={token}";
-    var apiLibraryUrl = "{baseuri}/library/metadata/{id}?X-Plex-Token={token}";
-    var downloadUrl = "{baseuri}{partkey}?download=1&X-Plex-Token={token}";
-    var accessTokenXpath = "//Device[@clientIdentifier='{clientid}']/@accessToken";
-    var baseUriXpath = "//Device[@clientIdentifier='{clientid}']/Connection[@local='0']/@uri";
-    var partKeyXpath = "//Media/Part[1]/@key";
-    var baseUri = null;
-    var accessToken = null;
+const plxDwnld = (function() {
+    "use strict";
 
-    var getXml = function(url, callback) {
-        var request = new XMLHttpRequest();
+    const self = {};
+    const clientIdRegex = new RegExp("server\/([a-f0-9]{40})\/");
+    const metadataIdRegex = new RegExp("key=%2Flibrary%2Fmetadata%2F(\\d+)");
+    const apiResourceUrl = "https://plex.tv/api/resources?includeHttps=1&X-Plex-Token={token}";
+    const apiLibraryUrl = "{baseuri}/library/metadata/{id}?X-Plex-Token={token}";
+    const downloadUrl = "{baseuri}{partkey}?download=1&X-Plex-Token={token}";
+    const accessTokenXpath = "//Device[@clientIdentifier='{clientid}']/@accessToken";
+    const baseUriXpath = "//Device[@clientIdentifier='{clientid}']/Connection[@local='0']/@uri";
+    const partKeyXpath = "//Media/Part[1]/@key";
+    let accessToken = null;
+    let baseUri = null;
+
+    const getXml = function(url, callback) {
+        const request = new XMLHttpRequest();
         request.onreadystatechange = function() {
             if (request.readyState == 4 && request.status == 200) {
                 callback(request.responseXML);
@@ -32,17 +34,17 @@
         request.send();
     };
 
-    var getMetadata = function(xml) {
-        var clientId = clientIdRegex.exec(window.location.href);
+    const getMetadata = function(xml) {
+        const clientId = clientIdRegex.exec(window.location.href);
 
         if (clientId && clientId.length == 2) {
-            var accessTokenNode = xml.evaluate(accessTokenXpath.replace('{clientid}', clientId[1]), xml, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
-            var baseUriNode = xml.evaluate(baseUriXpath.replace('{clientid}', clientId[1]), xml, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+            const accessTokenNode = xml.evaluate(accessTokenXpath.replace('{clientid}', clientId[1]), xml, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+            const baseUriNode = xml.evaluate(baseUriXpath.replace('{clientid}', clientId[1]), xml, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
 
             if (accessTokenNode.singleNodeValue && baseUriNode.singleNodeValue) {
                 accessToken = accessTokenNode.singleNodeValue.textContent;
                 baseUri = baseUriNode.singleNodeValue.textContent;
-                var metadataId = metadataIdRegex.exec(window.location.href);
+                const metadataId = metadataIdRegex.exec(window.location.href);
 
                 if (metadataId && metadataId.length == 2) {
                     getXml(apiLibraryUrl.replace('{baseuri}', baseUri).replace('{id}', metadataId[1]).replace('{token}', accessToken), getDownloadUrl);
@@ -57,8 +59,8 @@
         }
     };
 
-    var getDownloadUrl = function(xml) {
-        var partKeyNode = xml.evaluate(partKeyXpath, xml, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+    const getDownloadUrl = function(xml) {
+        const partKeyNode = xml.evaluate(partKeyXpath, xml, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
 
         if (partKeyNode.singleNodeValue) {
             window.location.href = downloadUrl.replace('{baseuri}', baseUri).replace('{partkey}', partKeyNode.singleNodeValue.textContent).replace('{token}', accessToken);
